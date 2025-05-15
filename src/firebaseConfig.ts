778d902276fbc89c15b974eea7d2f256392457b8
Sergey Labuzov <labuzov2003@gmail.com>
2025-05-15 14:05:04 +0300

    Add firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,19 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
+// public keys, .env is not needed
+const firebaseConfig: FirebaseOptions = {
+    apiKey: "AIzaSyDrvTV9JSaPFibX2DAHBDRv-60jKEJ7tLg",
+    authDomain: "q-time-app.firebaseapp.com",
+    projectId: "q-time-app",
+    storageBucket: "q-time-app.firebasestorage.app",
+    messagingSenderId: "500763083944",
+    appId: "1:500763083944:web:8eb05af08c79fec80e2e9d",
+    measurementId: "G-MJGGFRR54Y"
+}
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseFirestore = getFirestore(firebaseApp);
+export const firebaseAuth = getAuth(firebaseApp);
